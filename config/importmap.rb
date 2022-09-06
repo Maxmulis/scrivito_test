34a1e272acfb090013df33b1659d1494635ba3b9
Your Name Here <max.keller@justrelate.com>
2022-09-06 14:40:50 +0200

    initial commit

---
+++ config/importmap.rb
@@ -0,0 +1,18 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
+pin "scrivito", to: "https://ga.jspm.io/npm:scrivito@1.30.0/dist/index.browser.js"
+pin "@babel/runtime/helpers/esm/extends", to: "https://ga.jspm.io/npm:@babel/runtime@7.19.0/helpers/esm/extends.js"
+pin "history", to: "https://ga.jspm.io/npm:history@5.3.0/index.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.26/nodelibs/browser/process-production.js"
+pin "promise-polyfill", to: "https://ga.jspm.io/npm:promise-polyfill@8.2.3/src/index.js"
+pin "react", to: "https://ga.jspm.io/npm:react@18.2.0/index.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@18.2.0/index.js"
+pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.23.0/index.js"
+pin "speakingurl", to: "https://ga.jspm.io/npm:speakingurl@14.0.1/index.js"
+pin "tcomb", to: "https://ga.jspm.io/npm:tcomb@3.2.29/index.js"
+pin "tcomb-react", to: "https://ga.jspm.io/npm:tcomb-react@0.9.3/index.js"
+pin "tcomb-validation", to: "https://ga.jspm.io/npm:tcomb-validation@3.4.1/index.js"
+pin "underscore", to: "https://ga.jspm.io/npm:underscore@1.13.4/underscore-umd-min.js"
+pin "urijs", to: "https://ga.jspm.io/npm:urijs@1.19.11/src/URI.js"
