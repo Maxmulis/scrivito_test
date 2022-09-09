d9da9e7a488bd161c4324ec239c92f60a6f5c66d
Max Keller <48801952+Maxmulis@users.noreply.github.com>
2022-09-09 10:15:47 +0200

    add full-screen toggle

--- app/models/content_page.rb
+++ app/models/content_page.rb
@@ -1,9 +1,12 @@
 class ContentPage < Page
   attribute :hide_from_navigation, :enum, values: %w[visible hidden], default: 'visible'
-
+  attribute :full_screen, :enum, values: %w[no yes], default: 'no'
   # hide page from navigation
   def hidden_from_navigation?
     hide_from_navigation == 'hidden'
   end
 
+  def full_screen?
+    full_screen == 'yes'
+  end
 end
