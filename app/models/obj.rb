1c0e4dd31192ab3f26c128f635f2d868d4e0e98c
Max Keller <48801952+Maxmulis@users.noreply.github.com>
2022-09-15 13:15:30 +0200

    fix hide from navigation

--- app/models/obj.rb
+++ app/models/obj.rb
@@ -1,2 +1,13 @@
 class Obj < Scrivito::BasicObj
+  def method_missing(method, *args, &block)
+    if method.to_s == 'hidden_from_navigation?'
+      false
+    else
+      super
+    end
+  end
+
+  def respond_to_missing?(method, include_private = false)
+    method.to_s == 'hidden_from_navigation?' || super
+  end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -4,15 +4,5 @@ class Page < Obj
   attribute :child_order, :referencelist
   attribute :tags, :stringlist
 
-  def method_missing(method, *args, &block)
-    if method.to_s == 'hidden_from_navigation?'
-      false
-    else
-      super
-    end
-  end
-
-  def respond_to_missing?(method, include_private = false)
-    method.to_s == 'hidden_from_navigation?' || super
-  end
+  
 end
