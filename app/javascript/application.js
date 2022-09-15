efa30d0858717b05023c8ce54ad6bb0528cfe08d
Max Keller <48801952+Maxmulis@users.noreply.github.com>
2022-09-15 13:16:51 +0200

    make pages types selectable in content browser

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,36 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// import "@hotwired/turbo-rails"
-// import "controllers"
 //= require jquery
 //= require scrivito
 //= require bootstrap-sprockets
+const loadFilters = () => {
+  scrivito.on('load', function () {
+    scrivito.content_browser.filters = {
+      obj_class: {
+        field: '_obj_class',
+        options: {
+          images: {
+            title: 'Images',
+            value: 'Image',
+            icon: 'images'
+          },
+          pages: {
+            icon: 'copy',
+            options: {
+              content_pages: {
+                title: 'Content Pages',
+                value: 'ContentPage',
+                icon: 'sheet'
+              },
+              search_pages: {
+                title: 'Search Pages',
+                value: 'SearchPage',
+                icon: 'sheet'
+               }        
+            }
+          }
+        }
+      }
+    }
+  });
+}
+
+loadFilters();
